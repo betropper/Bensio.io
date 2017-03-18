61554d23201987c68ac9968ea369e61bf5d9a6c3
Benjamin Muhlestein <betropper@gmail.com>
2017-03-18 15:51:13 -0400

    save

--- js/game.js
+++ js/game.js
@@ -1,19 +1,50 @@
 var C = {
   game: {
-    width: 568,
-    height: 320
+    width: 1280,
+    height: 920
   },
-  backgrounds: {
-
-
+  background: {
+    width: 1280,
+    height: 920,
+    assets: {
+      "stars": "stars1.png",
+      "west": "west2.png",
+      "sunset": "sunset3.png"
+    }
   }
 }
 
 class Boot {
-  
+  init() {
+    console.log("%c||| Bootin' Bensio |||",
+                "color:#fdf6e3; background:#073642");
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically = true;
+  }
+  create() {
+    game.state.start('Load')
+  }
+}
+
+class Load {
+  preload() {
+    Object.keys(C.backgrounds.assets).forEach(function(assetName) {
+      game.load.image(assetName, C.background.assets[assetName]);
+    });
+  }
+  load() {
+    game.state.start('MainMenu');
+  }
+}
+
+class MainMenu {
+
 
 }
 
 
 game.state.add('Boot',Boot);
+game.state.add('Load',Load);
+game.state.add('MainMenu',MainMenu);
 var game = new Phaser.Game(C.game.width, C.game.height);
